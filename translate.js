65bc5775d07edb6b9cd60b5db7ea23a31858195d
twilightfox <80299794+twilightfoxx@users.noreply.github.com>
2021-09-30 17:52:45 -0400

    Update translate.js

--- translate.js
+++ translate.js
@@ -3,15 +3,6 @@
   
   const lmao = translate.split(/ +/); 
     
-    const isBritish = value => {
-    if(value.toLowerCase() === 'british') {
-     value = "Bri'ish"; 
-      alert(value);
-      } else { 
-       alert('why'); 
-        }
-    }
-    
- lmao.forEach(x => isBritish(x));
+   alert('im suck at progrem');
   
   }
